63ba6cf705c773fb0273ba16e68fb40b97883045
jackyzhang <852566802@qq.com>
2021-05-20 07:22:30 +0800

    4.7 练习题

--- ch4/ex4.7/rev.go
+++ ch4/ex4.7/rev.go
@@ -1,7 +1,19 @@
 package ex4_7
 
-func reverse(s []rune) {
+import "unicode/utf8"
+
+func reverse(s []byte) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
+}
+
+func reverseUTF8(s []byte) {
+	var size int
+	for i := 0; i < len(s); {
+		_, size = utf8.DecodeRune(s[i:])
+		reverse(s[i : i+size])
+		i += size
+	}
+	reverse(s)
 }
--- ch4/ex4.7/rev_test.go
+++ ch4/ex4.7/rev_test.go
@@ -3,10 +3,10 @@ package ex4_7
 import "fmt"
 
 func ExampleReverse() {
-	a := []rune{'a', '张', 'c', '~', 'e', 'f'}
-	reverse(a)
-	fmt.Printf("%c", a)
+	a := []byte("a张c~ef")
+	reverseUTF8(a)
+	fmt.Printf("%s", a)
 	//output:
-	//[f e ~ c 张 a]
+	//fe~c张a
 
 }
