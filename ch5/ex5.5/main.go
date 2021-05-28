977d659e916fd0a493f3a070f99ed23316ed8a68
zhangjianfei <123!@#qazABC>
2021-05-28 18:32:37 +0800

    ex5.5

---
+++ ch5/ex5.5/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+)
+
+func CountWordsAndImages(url string) (words, images int, err error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return
+	}
+	doc, err := html.Parse(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		err = fmt.Errorf("parsing HTML: %s", err)
+		return
+	}
+	words, images = countWordsAndImages(doc)
+	return
+}
+func countWordsAndImages(n *html.Node) (words, images int) { /* ... */
+	return
+}
+
+func main() {
+
+}
