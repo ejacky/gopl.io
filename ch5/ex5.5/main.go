f812dc1f1adac33c07ac54d32e36f5c6f094bbb8
zhangjianfei <123!@#qazABC>
2021-05-29 18:01:51 +0800

    finished ex5.5

--- ch5/ex5.5/main.go
+++ ch5/ex5.5/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"golang.org/x/net/html"
 	"net/http"
+	"os"
+	"strings"
 )
 
 func CountWordsAndImages(url string) (words, images int, err error) {
@@ -21,9 +24,35 @@ func CountWordsAndImages(url string) (words, images int, err error) {
 	return
 }
 func countWordsAndImages(n *html.Node) (words, images int) { /* ... */
+	if n.Type == html.ElementNode && n.Data == "img" {
+		images++
+	}
+	if n.Type == html.TextNode && !(n.Parent.Type == html.ElementNode && (n.Parent.Data == "style" || n.Parent.Data == "script") && len(n.Data) != 0) {
+		str := strings.TrimSpace(strings.Trim(n.Data, "\n"))
+		if len(str) > 0 {
+
+			input := bufio.NewScanner(strings.NewReader(str))
+			input.Split(bufio.ScanWords)
+
+			for input.Scan() {
+				words++
+			}
+
+			fmt.Println(str)
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		newWords, newImages := countWordsAndImages(c)
+		words += newWords
+		images += newImages
+	}
+
 	return
 }
 
 func main() {
-
+	for _, url := range os.Args[1:] {
+		words, images, _ := CountWordsAndImages(url)
+		fmt.Printf("words:%d\timages:%d\n", words, images)
+	}
 }
