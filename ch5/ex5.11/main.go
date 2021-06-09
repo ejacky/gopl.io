c39eeda22fb8e3ab37275f50e55f0a04f21e4970
jackyzhang <852566802@qq.com>
2021-06-10 07:30:23 +0800

    finished ex5.11

--- ch5/ex5.11/main.go
+++ ch5/ex5.11/main.go
@@ -13,23 +13,23 @@ import (
 //!+table
 // prereqs maps computer science courses to their prerequisites.
 var prereqs = map[string][]string{
-	//"algorithms": {"data structures"},
+	"algorithms":     {"data structures"},
 	"calculus":       {"linear algebra"},
 	"linear algebra": {"calculus"},
-	//
-	//"compilers": {
-	//	"data structures",
-	//	"formal languages",
-	//	"computer organization",
-	//},
-	//
-	//"data structures":       {"discrete math"},
-	//"databases":             {"data structures"},
-	//"discrete math":         {"intro to programming"},
-	//"formal languages":      {"discrete math"},
-	//"networks":              {"operating systems"},
-	//"operating systems":     {"data structures", "computer organization"},
-	//"programming languages": {"data structures", "computer organization"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
 }
 
 //!-table
@@ -41,7 +41,6 @@ func main() {
 
 	for i, course := range courses {
 		fmt.Printf("%d:\t%s\n", i+1, course)
-
 	}
 }
 
@@ -49,13 +48,22 @@ func topoSort(m map[string][]string) ([]string, bool) {
 	var order []string
 	var circle bool
 	seen := make(map[string]bool)
+	flag := make(map[string]bool)
 	var visitAll func(items []string)
 
 	visitAll = func(items []string) {
 		for _, course := range items {
+			if has, ok := flag[course]; ok && !has {
+				circle = true
+			}
+
 			if !seen[course] {
 				seen[course] = true
+
+				flag[course] = false
 				visitAll(m[course])
+				flag[course] = true
+
 				order = append(order, course)
 			}
 		}
