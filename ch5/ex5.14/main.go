478dbba822303c4c61dea40db5e333ac03056ff7
jackyzhang <852566802@qq.com>
2021-06-11 21:35:05 +0800

    finished ex5.14

---
+++ ch5/ex5.14/main.go
@@ -0,0 +1,66 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 136.
+
+// The toposort program prints the nodes of a DAG in topological order.
+package main
+
+import "fmt"
+
+//!+table
+// prereqs maps computer science courses to their prerequisites.
+var prereqs = map[string][]string{
+	"algorithms": {"data structures"},
+	"calculus":   {"linear algebra"},
+
+	"compilers": {
+		"data structures",
+		"formal languages",
+		"computer organization",
+	},
+
+	"data structures":       {"discrete math"},
+	"databases":             {"data structures"},
+	"discrete math":         {"intro to programming"},
+	"formal languages":      {"discrete math"},
+	"networks":              {"operating systems"},
+	"operating systems":     {"data structures", "computer organization"},
+	"programming languages": {"data structures", "computer organization"},
+}
+
+//!-table
+
+//!+main
+func main() {
+	breadthFirst(crawl, prereqs)
+}
+
+func breadthFirst(f func(item string) []string, m map[string][]string) []string {
+	var worklist []string
+	for key := range m {
+		worklist = append(worklist, key)
+	}
+
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+
+	return worklist
+}
+
+func crawl(course string) []string {
+	fmt.Println(course)
+
+	return prereqs[course]
+}
+
+//!-main
